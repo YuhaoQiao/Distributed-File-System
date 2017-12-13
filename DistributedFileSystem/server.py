5baf31bb07068ba8d67a8dd61c8498e3ae45c018
YuhaoQiao <33913866+YuhaoQiao@users.noreply.github.com>
2017-12-13 21:53:56 +0000

    Update server.py

--- DistributedFileSystem/server.py
+++ DistributedFileSystem/server.py
@@ -47,7 +47,55 @@ class ThreadPoolMixIn(SocketServer.ThreadingMixIn):
     =========================================================
                 THREAD INITIALIZATION FUNCTIONS
     =========================================================
+ """
+    #Main server loop
+    def serve_always(self):
+        #Create the request queue
+        self.request_queue = Queue.Queue(self.pool_size)
+        for t in range(self.pool_size):
+            t = threading.Thread(target = self.process_request_thread) #Initialize threads
+            #print "Starting pool thread ", t.name
+            t.start()
 
+        while 1:
+            self.handle_request() #Get the ball rolling
+
+    #Start handling the requests sent to the server
+    def handle_request(self):
+        #requests are essentially socket objects
+        request, client_address = self.get_request()
+        #Place in the queue
+        self.request_queue.put((request,client_address))
+
+    #Get a request from the queue
+    def process_request_thread(self):
+        while 1:
+            #ThreadingMixIn.process_request_thread(self, self.request_queue.get())
+            try:
+                request, client_address = self.request_queue.get()
+            except Queue.Empty:
+                pass
+            #Fufill request
+            self.finish_request(request, client_address)
+
+    """========================================================="""
+
+    """
+    =========================================================
+                    SOCKET OPERATIONS
+    =========================================================
+    """
+    #Every time a directory is changed, check which socket we have to send from
+    def resolve_socket(self, path):
+          #path = os.path.expanduser(path)
+          while path not in self.servers:
+            #Go up one directory
+            path = os.path.dirname(os.path.normpath(path))
+          
+          self.socketToSend = self.servers[path]
+          
+    
+    """========================================================="""
 """
     =========================================================
                     DIRECTORY OPERATIONS
@@ -156,3 +204,34 @@ class ThreadPoolMixIn(SocketServer.ThreadingMixIn):
 
 
     """========================================================="""
+    def shutdown(self):
+        server.server_close()
+        #Force shutdown
+        os._exit(os.EX_OK)
+
+
+class ThreadedRequestHandler(SocketServer.BaseRequestHandler):
+    pass
+
+    #Open a thread for each client and handle requests
+    """
+    def handle(self):
+        data = self.request.recv(1024)
+        curr_thread = threading.current_thread()
+        response = "{} - {}".format(curr_thread.name, data)
+        self.request.sendall(response)
+    """
+
+class Server(ThreadPoolMixIn, SocketServer.TCPServer):
+    pass
+
+if __name__ == "__main__":
+
+    HOST = "0.0.0.0"
+    PORT = int(sys.argv[1])
+    server = Server((HOST, PORT), ThreadedRequestHandler)
+    print "Server started - ", HOST, PORT
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.shutdown()
